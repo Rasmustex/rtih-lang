0162cc00cfa6d168df42323f48f2701eae9f60dc
Rasmus Hauge Hansen <rasmus14326@gmail.com>
2022-01-16 23:19:45 +0100

    Added memory checking for parser and tokenizer
    Modified-by: Rasmus Hauge Hansen <rasmus14326@gmail.com>

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@ only have an interpreted version
 #include <assert.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <unistd.h>
 #include <string.h>
 
 enum OP {
@@ -44,11 +45,11 @@ struct command {
 int sim( struct command *program );
 
 struct command *read_program_from_file( const char *fname );
-void print_help();
+void print_help( const char* progname );
 
 int main( int argc, const char **argv ) {
     if( argc < 2 ) {
-        print_help();
+        print_help( argv[0] );
         printf( "Error: no file name specified" );
         exit(1);
     }
@@ -67,7 +68,7 @@ void push( int argc, int args[10] );
 void plus();
 void minus();
 void dump();
-void exit_program( int argc, int args[10] );
+void exit_program();
 
 int sim( struct command *program ) {
     void (*op[NUM_OPS])( int argc, int args[10] );
@@ -162,7 +163,7 @@ inline void dump() {
     return;
 }
 
-inline void exit_program( int argc, int args[10] ) {
+inline void exit_program() {
     exit(POP_SIM);
     return;
 }
@@ -170,48 +171,62 @@ inline void exit_program( int argc, int args[10] ) {
 #define MAXTOK 100
 
 struct command *read_program_from_file( const char *fname ) {
-    FILE* f = fopen( fname, "r" );
-    int proglen = 10000;
-    TOK_TYPE tt;
-    char tok[MAXTOK];
+    uint64_t temp;
+    FILE* f;
+    if( !access( fname, F_OK ) ) {
+        f = fopen( fname, "r" ); // Open the requested file
+    } else {
+        printf( "Error: could not access file %s\n", fname );
+        exit(1);
+    }
+    size_t proglen = 10000; // Initial length of operation array
+    TOK_TYPE tt; // The type of token that the tokenizer is processing
+    char tok[MAXTOK]; // Holds the found token
     char *p = tok;
-    char c;
-    struct command *prog = malloc( proglen * sizeof( struct command ) );
+    register char c;
+    struct command *prog = (struct command*)malloc( proglen * sizeof( struct command ) ); // array of the commands that the program will execute
+    if( !prog ) {
+        printf( "Error: failed to allocate memory for program\n" );
+    }
     struct command *pp = prog;
 
-    while( !feof( f ) ) {
-        p = tok;
-        assert( pp - prog < proglen );
-        // feof(f)
+    while( 1 ) {
+        p = tok; // Reset p to the start of token string
+
+        /* TODO: Separate tokenizer to separate function - also make prettier. */
         while( (c = fgetc( f )) == ' ' || c == '\t' || c == '\n' )
             ;
         if( feof( f ) )
             break;
+        // Does token start with an alphabetic character? Then it must be a name
         if( isalpha( c ) ) {
             do {
-                *p++ = c;
-            } while( isalpha( c = fgetc(f) ) );
+                *p++ = c; // copy token to tok
+            } while( isalnum( c = fgetc(f) ) ); // Names are allowed to contain numbers
             tt = NAME;
-        } else if( isdigit(c) ) {
+        } else if( isdigit(c) ) { // If c is a digit, it must be part of a number that should be pushed to the stack
             do {
-                *p++ = c;
-            } while( isdigit( c = fgetc( f ) )) ;
+                *p++ = c; // copy token to toke
+            } while( isdigit( c = fgetc( f ) ) );
             tt = NUM;
-        } else {
+        } else { // Otherwise it must be an operator. This will probably change in the future
             tt = OP;
         }
-        *p = '\0';
+        *p = '\0'; // Null-terminate tok
         switch( tt ) {
-        case NUM:
+        case NUM: // If number, push
             *pp = push_op( atol(tok));
             break;
-        case NAME:
+        case NAME: // if name, check if exit, and then exit. Other names yet to be implemented
             if ( !strcmp( tok, "exit" ) ) {
                 *pp = exit_program_op();
+            } else {
+                printf( "Error: name %s not recognised, and custom names not implemented\n", tok );
+                exit(1);
             }
             break;
         default:
-            switch( c ) {
+            switch( c ) { // checks what was put into c
             case '+':
                 *pp = plus_op();
                 break;
@@ -220,17 +235,34 @@ struct command *read_program_from_file( const char *fname ) {
                 break;
             case '.':
                 *pp = dump_op();
+                break;
+            default:
+                printf( "Token %s is not recognised\n", tok );
+                exit(1);
             }
             break;
         }
         pp++;
+
+        if( !(pp - prog < proglen - 1) ) { // reallocate more memory for the program array if we run out - keep a buffer so we can always add the exit op at the end if the user hasn't
+            temp = pp - prog;
+            if( !(prog = ( struct command*)realloc( prog, (proglen += 1000) * sizeof( struct command ) ) ) ) {
+                printf( "Error: program memory reallocation failed\n" );
+                exit(1);
+            }
+            pp = prog + temp; // reset pp to the same position even if the array has moved in memory
+        }
     }
     fclose( f );
+    if( (pp - 1)->op != OP_EXIT ) {
+        *pp++ = push_op(0);
+        *pp = exit_program_op();
+    } // if there is no exit statement in the program, we just add one ourselves at the end
     return prog;
 }
 
-void print_help() {
-    printf("Usage: plang [ARGS]\n");
-    printf("    plang <filename>\truns the given file with the plang interpreter\n");
+void print_help( const char* progname ) {
+    printf( "Usage: plang [ARGS]\n" );
+    printf( "    %s <filename>\truns the given file with the plang interpreter\n", progname );
     return;
 }
